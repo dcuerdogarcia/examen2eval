0d9ca54161b1e881f72a9405bc81f9bd4a04a9ca
dcuerdogarcia <dcuerdogarcia@eniac10.danielcastelao.org>
2015-03-19 09:18:30 +0100

    Commit configuration builder

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -1,31 +1,44 @@
 package statustwitter;
 
+import twitter4j.Paging;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Clase para recobrar los twitts del morrazo
+ *
  * @author damian
  */
 public class StatusTwitter {
 
     /**
      * @param args the command line arguments
-     * @throws twitter4j.TwitterException
-     * Rama con sus respectivos cambios.
+     * @throws twitter4j.TwitterException Rama con sus respectivos cambios.
      */
     public static void main(String[] args) throws TwitterException {
-     
-        Twitter mitwitter = new TwitterFactory().getInstance();
- 
+
+        Twitter twitter;
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("Ako7UjY3M9qkf0yRlbgW31D3h")
+                .setOAuthConsumerSecret("WJ3GGGvYyUoSnV0Ag7RwDz8isExrcxNWNZZTozzTSnM2X1dXe4")
+                .setOAuthAccessToken("403449991-2vwfIjdgGOqQcHndqWRwJ8PHxLdHmV3LXYerlmH5")
+                .setOAuthAccessTokenSecret("zrQRIj6NDy9a7O1C1NUTvfbLdSvvl1fEPylZcCLDeqScw");
+        twitter = new TwitterFactory(cb.build()).getInstance();
+
+        Paging pagina = new Paging();
+
+//Twitter mitwitter = new TwitterFactory().getInstance();
         Query query = new Query("#Cangas");
-        QueryResult result = mitwitter.search(query);
+        QueryResult result = twitter.search(query);
         for (Status status : result.getTweets()) {
             System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
         }
     }
+
 }
